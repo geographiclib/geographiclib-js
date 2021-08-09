2a3c9cebde46cdac816bb23cca0f25ce8f5427d5
Hans Brende <hansbrende@apache.org>
2021-08-09 12:12:18 -0500

    typescript fixups

--- src/types/geographiclib.d.ts
+++ src/types/geographiclib.d.ts
@@ -83,6 +83,13 @@ declare class GeodesicClass {
     azi1: number;
     s12: number;
     a12: number;
+    lat2?: number
+    lon2?: number
+    azi2?: number
+    m12?: number
+    M12?: number
+    M21?: number
+    S12?: number
   };
 
   DirectLine(
@@ -130,6 +137,12 @@ declare class GeodesicClass {
     lon2: number;
     a12: number;
     s12?: number;
+    azi1?: number
+    azi2?: number
+    m12?: number
+    M12?: number
+    M21?: number
+    S12?: number
   }
 
   InverseLine(
@@ -139,9 +152,24 @@ declare class GeodesicClass {
     lon2: number,
     caps?: number
   ): any; // TODO: define GeodesicLine object
+
+  Polygon(polyline?: boolean): any // TODO: define PolygonArea object
 }
 
 export declare const Geodesic: {
+  NONE: number
+  ARC: number
+  LATITUDE: number
+  LONGITUDE: number
+  AZIMUTH: number
+  DISTANCE: number
+  STANDARD: number
+  DISTANCE_IN: number
+  REDUCEDLENGTH: number
+  GEODESICSCALE: number
+  AREA: number
+  ALL: number
+  LONG_UNROLL: number
   Geodesic: typeof GeodesicClass,
   WGS84: GeodesicClass
 };
