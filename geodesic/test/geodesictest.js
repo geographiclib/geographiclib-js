8b25b02c551f642f2add259101e622effd235e4f
Charles Karney <karney@alum.mit.edu>
2025-08-22 08:39:08 -0400

    Fix prolate meridional bug (test = GeodSolve100)

--- geodesic/test/geodesictest.js
+++ geodesic/test/geodesictest.js
@@ -661,6 +661,21 @@ describe("geodesic", function() {
       assert.approx(inv.s12, 19987083.007, 0.5e-3);
     });
 
+    it("GeodSolve100", function() {
+      // Check fix for meridional failure for a strongly prolate ellipsoid.
+      // This was caused by assuming that sig12 < 1 guarantees the meridional
+      // geodesic is shortest (even though m12 < 0).  Counter example is tested
+      // here.  Bug is not present for f >= -2, b < 3*a.  For f = -2.1 the
+      // inverse calculation for 30.61 0 30.61 180 exhibits the bug.
+      var geod = new g.Geodesic(1e6, -3),
+          inv = geod.Inverse(30, 0, 30, 180);
+      // Sloppy bounds checking because series solution is inaccurate for
+      // ellipsoids this eccentric.
+      assert.approx(inv.azi1,  22.368806, 1.0);
+      assert.approx(inv.azi2, 157.631194, 1.0);
+      assert.approx(inv.s12,   1074081.6, 1e3);
+    });
+
   });
 
   describe("Planimeter", function () {
